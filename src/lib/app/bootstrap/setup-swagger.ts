e0039dc3243bc5cff2d14097394d7ca107be3a4c
Ryan Holanda <ryanholanda08@gmail.com>
2026-01-15 12:36:11 -0300

    update swagger config

--- src/lib/app/bootstrap/setup-swagger.ts
+++ src/lib/app/bootstrap/setup-swagger.ts
@@ -8,12 +8,11 @@ export function setupSwagger(app: INestApplication) {
   const apiUrl = configService.get(EnvKey.API_URL);
 
   let configBuilder = new DocumentBuilder()
-    .setTitle(`Hydric Pools API`)
-    .setDescription(`Access liquidity pools from DEXs easily. One plug. Every pool. Every network.`)
+    .setTitle(`Hydric Gateway API`)
+    .setDescription(`Access DeFi Liquidity easily. One plug. Every Protocol. Every Network.`)
     .setVersion(process.env.npm_package_version || '1.0.0')
     .setOpenAPIVersion('3.1.0')
-    .addServer(apiUrl)
-    .addTag('System', 'Core infrastructure and health checks');
+    .addServer(apiUrl);
 
   const config = configBuilder.build();
 
