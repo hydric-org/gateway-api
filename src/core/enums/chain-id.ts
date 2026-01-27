86478fadc585b4087a69c1084e40698546caeb32
Ryan Holanda <ryanholanda08@gmail.com>
2026-01-27 15:03:43 -0300

    fix: remove unused network token

--- src/core/enums/chain-id.ts
+++ src/core/enums/chain-id.ts
@@ -1,6 +1,3 @@
-import { ISingleChainToken } from '../interfaces/token/single-chain-token.interface';
-import { NetworkToken, NetworkTokenUtils } from './network-token';
-
 export enum ChainId {
   ETHEREUM = 1,
   MONAD = 143,
@@ -19,16 +16,6 @@ export abstract class ChainIdUtils {
   static includes(chainId: number): boolean {
     return this.values().includes(chainId);
   }
-
-  static nativeToken: Record<ChainId, ISingleChainToken> = {
-    [ChainId.ETHEREUM]: { ...NetworkTokenUtils.metadata[NetworkToken.ETH], chainId: ChainId.ETHEREUM },
-    [ChainId.SCROLL]: { ...NetworkTokenUtils.metadata[NetworkToken.ETH], chainId: ChainId.SCROLL },
-    [ChainId.BASE]: { ...NetworkTokenUtils.metadata[NetworkToken.ETH], chainId: ChainId.BASE },
-    [ChainId.UNICHAIN]: { ...NetworkTokenUtils.metadata[NetworkToken.ETH], chainId: ChainId.UNICHAIN },
-    [ChainId.HYPER_EVM]: { ...NetworkTokenUtils.metadata[NetworkToken.HYPE], chainId: ChainId.HYPER_EVM },
-    [ChainId.PLASMA]: { ...NetworkTokenUtils.metadata[NetworkToken.XPL], chainId: ChainId.PLASMA },
-    [ChainId.MONAD]: { ...NetworkTokenUtils.metadata[NetworkToken.MON], chainId: ChainId.MONAD },
-  };
 
   static chainName: Record<ChainId, string> = {
     [ChainId.ETHEREUM]: 'Ethereum',
